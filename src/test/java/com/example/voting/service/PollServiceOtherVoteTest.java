9799d87ca9944f83a7325415e66d86087371419e
Dhruv Malik <138770923+Dm-1324@users.noreply.github.com>
2026-09-17 13:10:47 +0530

    fix Other vote test stubbing

--- src/test/java/com/example/voting/service/PollServiceOtherVoteTest.java
+++ src/test/java/com/example/voting/service/PollServiceOtherVoteTest.java
@@ -20,6 +20,8 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -46,6 +48,8 @@ class PollServiceOtherVoteTest {
         service = new PollService(pollRepository, optionRepository, voteRepository);
         ReflectionTestUtils.setField(service, "mainAdminUsername", "dhruvm1324");
         ReflectionTestUtils.setField(service, "mainAdminPassword", "admin");
+        lenient().when(voteRepository.findByPollIdAndOptionIdOrderByIdAsc(anyLong(), anyLong()))
+                .thenReturn(List.of());
     }
 
     @Test
